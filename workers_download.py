ae320dcc114519ece6cf2b58d4e3e42a6d84305f
Parth Chawla <pc.parth@gmail.com>
2022-05-10 21:57:30 -0700

    Update workers_download.py

--- workers_download.py
+++ workers_download.py
@@ -1,16 +1,52 @@
+import json
+import MySQLdb
 import pandas as pd
 
-# df = pd.DataFrame(columns = ["msr_id","msr_no","muster_roll_period_from","muster_roll_period_to","panchayat_code","payment_date","tot_persondays","total_dues","done"])
+def database_cursor():
 
-# chunksize = 10 ** 6
-# for chunk in pd.read_csv("/Users/parthchawla/musters.csv", chunksize=chunksize, names=["msr_id","msr_no","muster_roll_period_from","muster_roll_period_to","panchayat_code","payment_date","tot_persondays","total_dues","done"]):
-#     chunk["muster_roll_period_to"] = pd.to_datetime(chunk["muster_roll_period_to"])
-#     chunk["muster_roll_period_from"] = pd.to_datetime(chunk["muster_roll_period_from"])
-#     chunk = chunk[(chunk.muster_roll_period_to >= "2016-04-01") & (chunk.muster_roll_period_to <= "2016-04-30")]
-#     df = df.append(chunk)
+    with open('./secrets.json') as data_file:
+        CLIENT_SECRETS = json.load(data_file)
 
-# df.to_csv("/Users/parthchawla/musters_new.csv")
+    USERNAME = CLIENT_SECRETS['mysql2']['username']
+    PASSWORD = CLIENT_SECRETS['mysql2']['password']
+    DB_NAME = CLIENT_SECRETS['mysql2']['db_name']
 
-df = pd.read_csv("/Users/parthchawla/musters_new.csv")
-df["payment_date"] = pd.to_datetime(df["payment_date"])
-df.to_csv("/Users/parthchawla/musters_new1.csv")
+    db = MySQLdb.connect(host="fba-bank-db-backup.cgnvxq71e3jm.ap-south-1.rds.amazonaws.com",  # your host, usually localhost
+                         user=USERNAME,  # your username
+                         passwd=PASSWORD,  # your password
+                         db=DB_NAME,  # name of the data base
+                         charset='utf8',
+                         port=3306)
+    return [db, db.cursor()]
+
+
+def get_workers(msr_id):
+
+    db_cur = database_cursor()
+    db = db_cur[0]
+    df = pd.read_sql("""SELECT * FROM workers WHERE %s>0 LIMIT 1""" % msr_id, con=db)
+    db.close()
+
+    return df
+
+
+def chunker(lst, n):
+    return (lst[pos:pos + n] for pos in xrange(0, len(lst), n))
+
+
+if __name__ == '__main__':
+
+    df = pd.read_csv("/Users/parthchawla/musters_new.csv")
+    ids = df['msr_id'].tolist()
+    chunked = []
+
+    for chunk in chunker(ids, 10):
+        chunked.append(chunk)
+
+    for chunk in chunked[:2]:
+        
+
+    print chunked
+    exit()
+    workers = get_workers(1)
+    print workers
